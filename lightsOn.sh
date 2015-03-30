4959aae7ad34b66b5808674b89a7ce1c4a3f8b1b
Jamin W. Collins <jamin.collins@gmail.com>
2015-03-29 17:38:03 -0700

    Clean up comment wrapping
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- lightsOn.sh
+++ lightsOn.sh
@@ -15,17 +15,21 @@
 
 # HOW TO USE: Start the script with the number of seconds you want the checks
 # for fullscreen to be done. Example:
-# "./lightsOn.sh 120 &" will Check every 120 seconds if Mplayer, Minitube
-# VLC, Firefox or Chromium are fullscreen and delay screensaver and Power Management if so.
+# "./lightsOn.sh 120 &" will Check every 120 seconds if Mplayer, Minitube VLC,
+# Firefox or Chromium are fullscreen and delay screensaver and Power Management
+# if so.
 # You want the number of seconds to be ~10 seconds less than the time it takes
 # your screensaver or Power Management to activate.
 # If you don't pass an argument, the checks are done every 50 seconds.
 #
-# An optional array variable exists here to add the names of programs that will delay the screensaver if they're running.
-# This can be useful if you want to maintain a view of the program from a distance, like a music playlist for DJing,
-# or if the screensaver eats up CPU that chops into any background processes you have running,
-# such as realtime music programs like Ardour in MIDI keyboard mode.
-# If you use this feature, make sure you use the name of the binary of the program (which may exist, for instance, in /usr/bin).
+# An optional array variable exists here to add the names of programs that will
+# delay the screensaver if they're running.  This can be useful if you want to
+# maintain a view of the program from a distance, like a music playlist for
+# DJing, or if the screensaver eats up CPU that chops into any background
+# processes you have running, such as realtime music programs like Ardour in
+# MIDI keyboard mode.
+# If you use this feature, make sure you use the name of the binary of the
+# program (which may exist, for instance, in /usr/bin).
 
 
 # Modify these variables if you want this script to detect if Mplayer,
